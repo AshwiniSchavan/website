6d1acdf848ea658e00b994fe685096592444215a
Ashwini <ashvinich91@gmail.com>
2018-06-01 17:29:05 +0530

    some update

--- music/views.py
+++ music/views.py
@@ -1,53 +1,51 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 from .models import Album,Song
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 
 
-def index(request):
-    all_albums = Album.objects.all()
-    return render(request, 'music/index.html', {'all_albums': all_albums})
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
 
-    '''
-    ##############Database Display ##########
-    html = ' '
-    for album in all_albums :
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href= "' + url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
-'''
+    def get_queryset(self):
+        return Album.objects.all()
 
 
-def detail(request, album_id):
+class DetailView(generic.DetailView):
 
-        album = get_object_or_404(Album, pk=album_id)
+    model = Album
+    template_name = 'music/detail.html'
 
-        return render(request, 'music/detail.html', {'album': album})
+
+class AlbumCreate(CreateView):
+
+    model = Album
+    fields = ['artist', 'album_title ', ' genre', ' album_logo']
 
 
-def favorite(request,album_id):
+class AlbumUpdate(UpdateView):
 
-    album= get_object_or_404(Album, pk=album_id)
+    model = Album
+    fields = ['artist', 'album_title ', ' genre', ' album_logo']
+
+
+class AlbumDelete(DeleteView):
+
+    model = Album
+    success_url = reverse_lazy('music:index')
+
+
+def favorite(request, song_id):
+    song = get_object_or_404(Song, pk=song_id)
     try:
-        selected_song= album.song_set.get(pk=request.POST['song'])
-
-    except (KeyError, Song.DoesNotExit):
-        return render(request, 'music/detail.html', {
-            'album': album,
-            'error message': "You did not select valid song",
-        })
+        if song.is_favorite:
+            song.is_favorite = False
+        else:
+            song.is_favorite = True
+        song.save()
+    except (KeyError, Song.DoesNotExist):
+        return JsonResponse({'success': False})
     else:
-        selected_song.is_favorite =True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
-
-
-
-
-
-
-
-
-
-    # return HttpResponse("<h2> Details for Album id : "+str(album_id) + "</h2>")
-    
-
+        return JsonResponse({'success': True})
